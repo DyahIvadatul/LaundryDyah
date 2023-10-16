0204bdbfa9d9881a0b7fcf06c26d843ede7357a9
DyahIvadatul <dyahivadatullaila@gmail.com>
2023-10-16 11:32:07 +0700

    apk laundry ukk ( PBO )

---
+++ src/laundrydyah/koneksi.java
@@ -0,0 +1,25 @@
+package laundrydyah;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class koneksi {
+    private String url = "jdbc:mysql://localhost/laundry_dyah";
+    private String username = "root";
+    private String password = "";
+    private Connection con;
+    
+    public void connect(){
+        try {
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Koneksi Berhasil");
+        }catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
+    public Connection getCon() {
+        return con;
+    }
+}
